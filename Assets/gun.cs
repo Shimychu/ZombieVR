a91f000359ea8656e509db83f879dda06ffbc33d
Shimychu <phenizer@hotmail.com>
2020-11-24 15:18:23 -0800

    Added weapons and zombies
    Added weapon prefab and scripts.
    Added zombies prefab and ragdoll script.

---
+++ Assets/gun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    public float speed = 40;
+    public GameObject bullet;
+    public Transform barrel;
+    public AudioSource audioGun;
+    public AudioClip audioGunFire;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire()
+    {
+        GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+        audioGun.PlayOneShot(audioGunFire);
+        Destroy(spawnedBullet, 2);
+    }
+}
---
+++ Assets/zombie.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie : MonoBehaviour
+{
+
+    private Rigidbody[] rbs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbs = GetComponentsInChildren<Rigidbody>();
+        DeactivateRageDoll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ActivateRageDoll()
+    {
+        foreach (var item in rbs)
+        {
+            item.isKinematic = false;
+        }
+    }
+
+    void DeactivateRageDoll()
+    {
+        foreach (var item in rbs)
+        {
+            item.isKinematic = true;
+        }
+    }
+}
