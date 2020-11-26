1861122535858243d8e77a25403dd11f84752a40
Shimychu <phenizer@hotmail.com>
2020-11-25 17:43:05 -0800

    Updated Zombie hitbox
    Zombie will now die after getting hit in the head.

--- Assets/zombie.cs
+++ Assets/zombie.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 using UnityEngine.AI;
 
 public class zombie : MonoBehaviour
 {
+
     public Transform target;
     private NavMeshAgent agent;
 
@@ -45,5 +45,33 @@ public class zombie : MonoBehaviour
         }
     }
 
+    public void Death()
+    {
+        ActivateRageDoll();
+        agent.enabled = false;
+        GetComponent<Animator>().enabled = false;
+        Destroy(this);
+    }
+
+
+    public void Hit()
+    {
+        Death();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Weapon"))
+        {
+            Hit();
+            Debug.Log("HIT");
+        }
+        else
+        {
+            Debug.Log("NO HIT");
+        }
+
+         
+    }
 
 }
