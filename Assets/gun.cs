f1e197a496c3d8f1ca049ea7e5a7a567e9fc2954
Shimychu <phenizer@hotmail.com>
2020-12-09 15:24:05 -0800

    Added gun ammo ui onto gun

--- Assets/gun.cs
+++ Assets/gun.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class gun : MonoBehaviour
 {
+
+    public const int MAX_AMMO = 12;
     public float speed = 40;
+
+    public TMPro.TextMeshProUGUI GunText;
     public GameObject bullet;
     public Transform barrel;
     public AudioSource audioGun;
     public AudioClip audioGunFire;
     public AudioClip audioGunEmpty;
-    public int bulletCount = 12;
+    public int bulletCount = MAX_AMMO;
 
 
 
@@ -23,14 +27,19 @@ public class gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Angle(transform.up, Vector3.up) > 100)
+        {
+            Reload();
+        }
+        GunText.text = bulletCount.ToString();
     }
 
     public void Fire()
     {
-        bulletCount--;
-        if(bulletCount >= 0)
+        
+        if(bulletCount > 0)
         {
+            bulletCount--;
             GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
             spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
             audioGun.PlayOneShot(audioGunFire);
@@ -39,5 +48,11 @@ public class gun : MonoBehaviour
         {
             audioGun.PlayOneShot(audioGunEmpty);
         }
+    }
+
+    void Reload()
+    {
+        bulletCount = MAX_AMMO;
+        
     }
 }
