5233a5665dc29f3bd1bb4a7a6bc6e46ffc4789de
Shimychu <phenizer@hotmail.com>
2020-12-28 15:55:07 -0800

    Added menu scene
    Added interactable menu scene & loades into game scene.

---
+++ Assets/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private int newScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene newScene = SceneManager.CreateScene("Game");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadNextScene()
+    {
+        
+        SceneManager.LoadScene(newScene);
+    }
+}
