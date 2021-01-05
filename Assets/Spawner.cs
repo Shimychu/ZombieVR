41e430beff6254a196bdac8af53e8e483a1cdee9
Shimychu <phenizer@hotmail.com>
2021-01-04 17:12:09 -0800

    Added opening bells and timed spawners
    Opening bells indicates a start of a wave. Zombie spawns in 4 location after 1 second intervals. Maximum zombie spawn inital wave is 10.

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -9,23 +9,46 @@ public class Spawner : MonoBehaviour
     public GameObject spawnGameObject;
     public Transform[] spawnPoints;
     private float timer;
+    private bool nextWave = false;
+    private int totalZombies = 10;
+    public AudioClip openingAudio;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextWave = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timer > spawnTime)
+        if(nextWave)
+        {
+            nextWave = false;
+            StartCoroutine(WaitOpening());
+            StartCoroutine(WaitZombie());
+        }
+    }
+
+    IEnumerator WaitZombie()
+    {
+        yield return new WaitForSeconds(6);
+        for (int i = 0; i < totalZombies; i++)
         {
             Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
             Instantiate(spawnGameObject, randomPoint.position, randomPoint.rotation);
-            timer = 0;
+            yield return new WaitForSeconds(2); 
         }
+        totalZombies = totalZombies + 10;
+    }
 
-        timer += Time.deltaTime;
+    IEnumerator WaitOpening()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            GetComponent<AudioSource>().PlayOneShot(openingAudio);
+            yield return new WaitForSecondsRealtime(2);
+        }
+        Debug.Log("waiting 3 seconds");
     }
 }
